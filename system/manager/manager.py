30c859c325626dd7031ae1ab45f043a8c164bbcb
rav4kumar <meetkumardesai@gmail.com>
2025-01-25 10:20:16 -0700

    drive mode selector

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -59,6 +59,7 @@ def manager_init() -> None:
     ("ToyotaEnhancedBsm", "0"),
     ("ToyotaTSS2Long", "0"),
     ("AccelPersonality", str(custom.LongitudinalPlanSP.AccelerationPersonality.stock)),
+    ("ToyotaDriveMode", "0"),
   ]
 
   if params.get_bool("RecordFrontLock"):
