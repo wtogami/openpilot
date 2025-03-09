0bd425f7d01abdd394998d30c7d320c51ea97c37
rav4kumar <meetkumardesai@gmail.com>
2025-03-09 15:59:44 -0700

    init accel personality

--- common/params_keys.h
+++ common/params_keys.h
@@ -174,4 +174,5 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"ToyotaEnhancedBsm", PERSISTENT},
     {"ToyotaTSS2Long", PERSISTENT},
     {"RainbowMode", PERSISTENT},
+    {"AccelPersonality", PERSISTENT},
 };
