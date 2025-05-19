013225b4c2a21eea7c383d6e37313bd59b74e646
rav4kumar <meetkumardesai@gmail.com>
2025-05-18 17:16:41 -0700

    acc-speed-increment-cereal

--- selfdrive/car/card.py
+++ selfdrive/car/card.py
@@ -187,7 +187,7 @@ class Car:
     self.params.put_nonblocking("CarParamsSPPersistent", cp_sp_bytes)
 
     self.mock_carstate = MockCarState()
-    self.v_cruise_helper = VCruiseHelper(self.CP)
+    self.v_cruise_helper = VCruiseHelper(self.CP, self.CP_SP)
 
     self.is_metric = self.params.get_bool("IsMetric")
     self.experimental_mode = self.params.get_bool("ExperimentalMode")
@@ -310,6 +310,7 @@ class Car:
 
       # sunnypilot
       self.dynamic_experimental_control = self.params.get_bool("DynamicExperimentalControl")
+      sunnypilot_interfaces._custom_acc_controls(self.CP_SP, self.params)
 
       time.sleep(0.1)
 
