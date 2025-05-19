e48ad777417ca88d3556c0d1c45a23b70998bbd6
rav4kumar <meetkumardesai@gmail.com>
2025-05-18 17:07:33 -0700

    tune

--- sunnypilot/selfdrive/controls/lib/dynamic_personality/dynamic_personality_controller.py
+++ sunnypilot/selfdrive/controls/lib/dynamic_personality/dynamic_personality_controller.py
@@ -55,14 +55,14 @@ class DynamicPersonalityController:
         float: The calculated follow distance factor
     """
     if personality == log.LongitudinalPersonality.relaxed:
-      x_vel =  [0., 8., 40.]
-      y_dist = [1.25, 1.25, 1.75]
+      x_vel =  [0.,  11.1, 13., 19.7, 22.2, 22.7, 40.]
+      y_dist = [1.20, 1.20, 1.50, 1.50, 1.60, 1.75, 1.75]
     elif personality == log.LongitudinalPersonality.standard:
-      x_vel =  [0., 8., 40.]
-      y_dist = [1.20, 1.20, 1.50]
+      x_vel =  [0.,   6.,   7.,   14.,  14.5, 19.7, 20.2, 25.0, 40.]
+      y_dist = [1.10, 1.10, 1.15, 1.15, 1.30, 1.30, 1.45, 1.45, 1.50]
     elif personality == log.LongitudinalPersonality.aggressive:
-      x_vel =  [0., 5., 13., 40.]
-      y_dist = [1.18, 1.18, 1.15, 1.25]
+      x_vel =  [0.,   6.,   7.,   14.,  14.5, 19.7, 20.2, 25.0, 40.]
+      y_dist = [1.05, 1.05, 1.15, 1.15, 1.20, 1.20, 1.25, 1.25, 1.35]
     else:
       raise NotImplementedError("Dynamic personality not supported")
 
