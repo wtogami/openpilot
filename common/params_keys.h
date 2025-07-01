c7a3b1f7f076d1c399059418c0b88ca455684f92
discountchubbs <alexgrant990@gmail.com>
2025-06-30 21:42:36 -0700

    Lagd toggle:
    Display current values on UI

--- common/params_keys.h
+++ common/params_keys.h
@@ -179,6 +179,7 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
 
     // model panel params
     {"LagdToggle", PERSISTENT | BACKUP},
+    {"LagdToggleDesc", PERSISTENT},
 
     // mapd
     {"MapAdvisorySpeedLimit", CLEAR_ON_ONROAD_TRANSITION},
